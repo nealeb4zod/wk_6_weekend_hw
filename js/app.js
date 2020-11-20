43300eb47822f2d35a1712a73de40e9e3e87cc49
Neale Johnston <neale@team-johnston.com>
2020-11-20 17:59:44 +0000

    Delete all working!

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const submitForm = document.querySelector("#add-instructor");
   submitForm.addEventListener("submit", handleSubmitForm);
+
+  const deleteAll = document.querySelector("#delete-all");
+  deleteAll.addEventListener("click", handleDeleteAll);
 });
+
+const handleDeleteAll = function () {
+  const instructorList = document.querySelector("#list");
+  instructorList.innerHTML = "";
+};
 
 const handleSubmitForm = function (event) {
   event.preventDefault();
-
   const newInstructor = createNewInstructor(event.target);
   const instructorListElement = newInstructor.makeInstructorListElement(
     ["h2", "h2", "h3", "h3"],
@@ -14,12 +21,10 @@ const handleSubmitForm = function (event) {
   );
   const instructorList = document.querySelector("#list");
   instructorList.appendChild(instructorListElement);
-
   event.target.reset();
 };
 
 const createNewInstructor = function (form_input) {
-  console.log(form_input);
   const instructor = new Instructor(
     form_input.first_name.value,
     form_input.last_name.value,
